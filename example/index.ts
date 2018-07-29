61379e0efdee9d7bcae4bd2218c19e3ac5486c1b
saqsun <sargsyan.sargis.89@gmail.com>
2018-07-29 21:07:08 +0400

    more examples are added

--- example/index.ts
+++ example/index.ts
@@ -2,6 +2,7 @@
 
 import "phaser";
 import { NinePatchPlugin } from "../src/com/koreez/plugin/NinePatchPlugin";
+import { NinePatch } from "../src/com/koreez/plugin/ninepatch/NinePatch";
 
 export default class Game extends Phaser.Game {
     public centerX: number;
@@ -15,35 +16,65 @@ function preload() {
     console.log("preload");
     this.load.image("metalPanel_blueCorner", "assets/metalPanel_blueCorner.png");
     this.load.image("squareGreen", "assets/squareGreen.png");
+    this.load.image("squareYellow", "assets/squareYellow.png");
 }
 
 function create() {
     console.log("create");
+    // We specify the x and y position, the width and height and the key for the image of the image.
+    // It will be automatically scaled!
     this.add.ninePatch(200, 200, 300, 300, "metalPanel_blueCorner", null, {
         bottom: 11, // Amount of pixels for bottom
         left: 11, // Amount of pixels for left
         right: 53, // Amount of pixels for right
         top: 32 // Amount of pixels for top
     });
-    this.add.ninePatch(60, 400, 100, 50, "squareGreen", null, {
-        bottom: 14, // Amount of pixels for bottom
-        left: 6, // Amount of pixels for left
-        right: 6, // Amount of pixels for right
-        top: 10 // Amount of pixels for top
-    });
-    this.add.ninePatch(230, 400, 200, 50, "squareGreen", null, {
-        bottom: 14, // Amount of pixels for bottom
-        left: 6, // Amount of pixels for left
-        right: 6, // Amount of pixels for right
-        top: 10 // Amount of pixels for top
+
+    let ninePatch = this.make.ninePatch({
+        /* tslint:disable */
+        key: "squareGreen",
+        width: 100,
+        height: 50,
+        patchesConfig: {
+            bottom: 14, // Amount of pixels for bottom
+            left: 6, // Amount of pixels for left
+            right: 6, // Amount of pixels for right
+            top: 10 // Amount of pixels for top
+        }
+        /* tslint:enable */
     });
 
-    this.add.ninePatch(200, 500, 300, 100, "squareGreen", null, {
+    ninePatch.x = 60;
+    ninePatch.y = 400;
+    this.add.existing(ninePatch);
+
+    // Or we use the Constructor
+    ninePatch = new NinePatch(this, 230, 400, 200, 50, "squareGreen", null, {
+        /* tslint:disable */
         bottom: 14, // Amount of pixels for bottom
         left: 6, // Amount of pixels for left
         right: 6, // Amount of pixels for right
         top: 10 // Amount of pixels for top
+        /* tslint:enable */
     });
+    ninePatch.x = 230;
+    ninePatch.y = 400;
+    this.add.existing(ninePatch);
+
+    setTimeout(() => {
+        ninePatch.resize(250, 60);
+    }, 2000);
+
+    this.cache.custom.ninePatch.add("squareYellow", {
+        /* tslint:disable */
+        bottom: 14, // Amount of pixels for bottom
+        left: 6, // Amount of pixels for left
+        right: 6, // Amount of pixels for right
+        top: 10 // Amount of pixels for top
+        /* tslint:enable */
+    });
+
+    this.add.ninePatch(200, 500, 300, 100, "squareYellow");
 }
 
 document.onreadystatechange = () => {
