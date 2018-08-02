a8cff72f516f81d9420b4ebfd239d1c4fe035544
RollinSafary <30594036+RollinSafary@users.noreply.github.com>
2018-08-02 11:06:29 +0400

    addToScene made optional

--- src/com/koreez/plugin/ninepatch/INinePatchCreator.ts
+++ src/com/koreez/plugin/ninepatch/INinePatchCreator.ts
@@ -1,5 +1,5 @@
 import { NinePatch } from "./NinePatch";
 
 export interface INinePatchCreator extends Phaser.GameObjects.GameObjectCreator {
-    ninePatch: (config: any, addToScene: boolean) => NinePatch;
+    ninePatch: (config: any, addToScene?: boolean) => NinePatch;
 }
